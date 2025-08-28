825a6a76c1e4b5794ba528780aad8ae3d50cdf93
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2025-08-28 20:50:53 -0300

    Corrige lista de exceção de recursos.

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('registerUsers', RegisterUserController::class)->except([
-    'create', 'show'
+    'create', 'edit'
 ]);
 
 Route::apiResource('profiles', ProfileController::class)->except([
